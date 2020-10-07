19c9696626ab60c62cd93e5d92eda16f67d10c36
Sarah Ridge <ridge.sarah@gmail.com>
2020-10-06 21:45:36 -0700

    Remove Instance Variable from Balances Controller

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
 
 class BalancesController < ApplicationController
-  before_action :set_organization
-
   def show
-    return head :bad_request if @organization.blank?
+    return head :bad_request if organization.blank?
 
-    render json: CreditBalanceResult.new(organization: @organization).perform
+    render json: CreditBalanceResult.new(organization: organization).perform
   rescue NoMethodError => _e
     render json: { error: "Could not retrieve balance" }, status: :unprocessable_entity
   end
 
   private
 
-  def set_organization
-    @organization = Organization.find_by(slug: params[:org_slug])
+  def organization
+    @organization ||= Organization.find_by(slug: params[:org_slug])
   end
 end
